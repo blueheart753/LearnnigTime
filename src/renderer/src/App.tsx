026aff5865bb03b4d544e77beaabdae2b090a07d
kimminam <bluemind8520@gmail.com>
2024-12-30 21:00:57 +0900

    Feat. Add Router StudyRoom

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import DashBoard from './pages/DashBoard';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/DashBoard" element={<DashBoard />} />
       </Routes>
     </>
   );
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DashBoard from './pages/DashBoard';
+import StudyRoom from './pages/StudyRoom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/DashBoard" element={<DashBoard />} />
+        <Route path="/StudyRoom" element={<StudyRoom />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
