caab07188db36069de7f442410562a7563d250f2
kimminam <bluemind8520@gmail.com>
2025-02-08 23:38:14 +0900

    Hotfix. BrowserRouter -> HashRouter BrowserRouter로 사용하면 Preview 및 빌드 시 화면이 재대로 출력이 되지 않는 버그해결결

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,18 +3,18 @@ import './assets/main.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import DashBoard from './pages/DashBoard';
 import StudyRoom from './pages/StudyRoom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/DashBoard" element={<DashBoard />} />
         <Route path="/StudyRoom" element={<StudyRoom />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
