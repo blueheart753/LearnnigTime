efb5cc5024b980d5e362265bb0572c6aecdf245f
kimminam <bluemind8520@gmail.com>
2025-02-19 20:53:39 +0900

    자동 업데이트 도입

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,8 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
 
 function createWindow(): void {
   // Create the browser window.
@@ -16,6 +18,28 @@ function createWindow(): void {
       sandbox: false
     }
   })
+
+  autoUpdater.on('checking-for-update', () => {
+    log.info('업데이트 확인 중...');
+  });
+  autoUpdater.on('update-available', (info) => {
+    log.info('업데이트가 가능합니다.');
+  });
+  autoUpdater.on('update-not-available', (info) => {
+    log.info('현재 최신버전입니다.');
+  });
+  autoUpdater.on('error', (err) => {
+    log.info('에러가 발생하였습니다. 에러내용 : ' + err);
+  });
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    log.info(log_message);
+  })
+  autoUpdater.on('update-downloaded', (info) => {
+    log.info('업데이트가 완료되었습니다.');
+  });
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
@@ -53,7 +77,7 @@ app.whenReady().then(() => {
   ipcMain.on('ping', () => console.log('pong'))
 
   createWindow()
-
+  autoUpdater.checkForUpdates();
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
